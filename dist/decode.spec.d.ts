458a1ccb028f4dacb2044e24e39c0f08d2e539a9
Wes Cravens <wcravens@cortex-it.com>
2023-12-18 02:02:50 -0600

    Remove dist files from git

--- dist/decode.spec.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=decode.spec.d.ts.map
--- dist/validationMatrix.d.ts
+++
@@ -1,8 +0,0 @@
-import { StreamReading } from "./streamReading";
-export declare const validationMatrix: {
-    deviceModel: string;
-    mfgData: string;
-    address: string;
-    streamReading?: StreamReading;
-}[];
-//# sourceMappingURL=validationMatrix.d.ts.map
